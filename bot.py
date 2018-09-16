1d4a86254d1e20e6259b974b92f8f6628328bb11
kingcrypto0001 <43308797+kingcrypto0001@users.noreply.github.com>
2018-09-16 11:58:21 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import json
 import time
 import os
 
-TOKEN = os.environ["SuperBot_TOKEN"]
+TOKEN = "MzkwMzI5NzE5ODcyMTU5NzY1.Dnxfsw.-IPj66ctnLzZnMrGTdUI8jZnGnU"
 VERSION = "v0.05"
 PREFIX = ","
 OWNERID = ["248242789169496064", "307934179738386452"] #ME, Astraqa
